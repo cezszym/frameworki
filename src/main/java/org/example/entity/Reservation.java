03d3e2750ca4ec4ebec01016860c4eca24e66ccd
OPest <j.sieczka99@wp.pl>
2022-04-04 18:44:27 +0200

    reservation entity created

--- src/main/java/org/example/entity/Reservation.java
+++ src/main/java/org/example/entity/Reservation.java
@@ -9,12 +9,22 @@ import java.util.Date;
 import java.util.UUID;
 
 @Entity
+@Table(name = "reservation")
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    private UUID postId;
-    private UUID userId;
+
+    // Relacja 'wiele rezerwacji do pojedynczych postów'
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    // Relacja 'wiele rezerwacji do pojedynczych użytkowników'
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     private Date startDate;
     private Date endDate;
     private ReservationStatus status;
