27945ac3065793755c71e845c924d44705e83b9b
Mykola Ostapchuk <kokola33258@gmail.com>
2022-04-04 18:49:12 +0200

    Add Review.java + Delete Comment.java

---
+++ src/main/java/org/example/entity/Review.java
@@ -0,0 +1,26 @@
+package org.example.entity;
+
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class Opinion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String contents;
+    private Date exposureDate;
+    private Integer likes;
+    private Integer dislikes;
+}
