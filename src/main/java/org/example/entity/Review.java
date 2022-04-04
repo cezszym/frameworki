97bd6ef9d04449cfc9d611c7f519d41203b26d15
Mykola Ostapchuk <kokola33258@gmail.com>
2022-04-04 19:05:07 +0200

    Add projectlombok to Review.java

--- src/main/java/org/example/entity/Review.java
+++ src/main/java/org/example/entity/Review.java
@@ -1,25 +1,45 @@
 package org.example.entity;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Review {
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @ManyToOne
+    @NotNull
     @JoinColumn(name = "post_id")
     private Post post;
 
     @ManyToOne
+    @NotNull
     @JoinColumn(name = "user_id")
     private User user;
 
+    @NotNull
     private String contents;
+
+    @NotNull
     private Date exposureDate;
+
+    @NotNull
     private Integer likes;
+
+    @NotNull
     private Integer dislikes;
 }
